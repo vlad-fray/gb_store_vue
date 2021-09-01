e4f9b9b99473704a0a64d20c7721985a6a0a93d0
VFray <rekrut313@gmail.com>
2021-09-01 12:05:09 +0300

    modal component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <Header @openCart="openCart" />
 
-  <teleport to=".modals" v-if="isCartOpened">
-    <div class="modal">
-      <Cart @closeCart="closeCart" :data="{ cart }" />
-    </div>
-    <div @click="closeCart" class="overlay"></div>
-  </teleport>
+  <Modal @close="closeCart" v-if="isCartOpened">
+    <Cart @closeCart="closeCart" :data="{ cart }" />
+  </Modal>
 
-  <Catalog :goods="goods" />
+  <!-- <Catalog :goods="goods" /> -->
 </template>
 
 <script>
@@ -16,14 +13,15 @@ import "./assets/base-styles.css";
 import Header from "./layouts/Header.vue";
 import Catalog from "./layouts/Catalog.vue";
 import Cart from "./components/Cart.vue";
+import Modal from "./UI/Modal.vue";
 import * as store from "./store/store.js";
-import { API } from "./store/config.js";
+import { API } from "./config.js";
 
-const init = async () => {
-  await store.loadCatalogItemsFromDatabase(`${API}/goodsList.json`);
-  console.log(store.state);
-};
-init();
+// const init = async () => {
+//   await store.loadCatalogItemsFromDatabase(`${API}/goodsList.json`);
+//   console.log(store.state);
+// };
+// init();
 
 export default {
   name: "App",
@@ -31,6 +29,7 @@ export default {
     Header,
     Cart,
     Catalog,
+    Modal,
   },
   data() {
     console.log(store.state.goods);
---
+++ src/UI/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <teleport to=".modals">
+    <div class="modal">
+      <slot></slot>
+    </div>
+    <div @click="closeModal" class="overlay"></div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.overlay {
+  content: "";
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #00000080;
+}
+
+::-webkit-scrollbar {
+  width: 0px;
+  background: transparent;
+}
+
+.modal {
+  content: "";
+  position: fixed;
+  z-index: 2;
+  left: 50%;
+  top: 10%;
+  transform: translateX(-50%);
+  width: 80%;
+  max-width: 1600px;
+  max-height: 80%;
+  overflow-y: scroll;
+}
+</style>
